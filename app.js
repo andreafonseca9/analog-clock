619f04dc635cc620ff3a7a8f9eb4de5008314b64
andreafonseca9 <110311284+andreafonseca9@users.noreply.github.com>
2022-08-15 01:45:29 -0700

    3rd commit

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const secondsHand = document.getElementById('seconds-hand')
-const minutesHand = document.getElementById('minutes-hand')
-const hoursHand = document.getElementById('hours-hand')
+const secondsHand = document.getElementById("seconds-hand");
+const minutesHand = document.getElementById("minutes-hand");
+const hoursHand = document.getElementById("hours-hand");
 
-function getTime(){
-    const now=new Date()
-    const seconds=now.getSeconds()
-    const minutes=now.getMinutes()
-    const hours=now.getHours()
+function getTime() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+  const timeInterval = 6;
 
-    console.log(now)
+  secondsHand.style.transform = "rotate(" + (seconds + timeInterval) + "deg)";
+  minutesHand.style.transform =
+    "rotate(" + (minutes + timeInterval + seconds / 10) + "deg)";
+  hoursHand.style.transform = "rotate(" + (hours + 30 + minutes / 2) + "deg)";
 }
-getTime()
+setInterval(getTime, 100);
